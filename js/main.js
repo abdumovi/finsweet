7b6c9eb924cb12cb24a936999e2ab50696ac567a
abdumovi <abdumovi@gmail.com>
2021-09-26 14:01:04 +0500

    links-add

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
-let btn = document.querySelector(".btn");
+let btn = document.querySelector(".hero__btn");
 let modal = document.querySelector(".modal");
 let modalBtn = document.querySelector(".modal-btn");
-
 modalBtn.addEventListener('click',function(){
   modal.classList.remove('open');
-  
 })
 
 btn.addEventListener('click',function (){
